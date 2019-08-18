34893d56d1439be1bcf5857ac80d9dcf7c54abdf
changewade <changewade@icloud.com>
2019-08-18 19:47:04 +1000

    完善week1

--- 浙江大学C语言基础/第1周：计算/change.c
+++ 浙江大学C语言基础/第1周：计算/change.c
@@ -1,23 +1,43 @@
-//
-//  first week.c
-//  
-//
-//  Created by 陈志春洋 on 2019/8/18.
-//
-
-#include "change.h"
 #include <stdio.h>
+
+//AMOUNT为常量
+/*
 
 int main()
 {
+	//定义常量
+	const int AMOUNT = 100;
+    //定义变量
     int price = 0;
     
     printf("请输入金额（元)");
     scanf("%d", &price);
     
-    int change = 100 - price;
+    int change = AMOUNT - price;
     
-    printf("找您%d元\n", change);
+    printf("找您%d元。\n", change);
+    
+    return 0;
+}
+
+*/
+
+//AMOUNT为用户输入量
+
+int main()
+{
+    //定义变量
+    int amount = 0;
+    int price = 0;
+
+    printf("请输入收款金额（元)");
+    scanf("%d", &amount);
+    printf("请输入货品金额（元)");
+    scanf("%d", &price);
+    
+    int change = amount - price;
+    
+    printf("找您%d元。\n", change);
     
     return 0;
 }
